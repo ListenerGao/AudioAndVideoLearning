8edb9179059a5ef11f2c1582047d027c0448ce80
Listener Gao <gaoyashen@jd.com>
2021-01-24 16:43:28 +0800

    更新示例

---
+++ lamemp3/src/main/java/com/listenergao/lamemp3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.listenergao.lamemp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * create on 18/10/14
+ *
+ * @author listenergao
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ lamemp3/src/main/java/com/listenergao/lamemp3/Mp3Encoder.java
@@ -0,0 +1,11 @@
+package com.listenergao.lamemp3;
+
+/**
+ * create on 2018/10/14
+ *
+ * @author ListenerGao
+ */
+public class Mp3Encoder {
+
+    public native void encode();
+}
